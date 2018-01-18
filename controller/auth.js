a82fd1bb687a3b732ce1e6b16d93dceb8a96dd03
suwings <suwings@outlook.com>
2018-01-18 16:33:35 +0800

    修改初始目录为当前目录

--- controller/auth.js
+++ controller/auth.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
-const {
-    parseHandle,
-    sendHandle,
-    filesToPaths
-} = require("../module/dataHandle");
 const {
     FileOperateStructure,
 } = require("../model/fsoperate_session");
 
+const BASE_DIR = process.cwd();
 
 router.all('/', (req, res) => {
-    req.session.fsos = new FileOperateStructure("E:/test/", "./");
+    req.session.fsos = new FileOperateStructure(BASE_DIR, "./");
     req.session.fsoperate = {};
     req.session.fsoperate.tmp = [];
     req.session.w = "AAA";

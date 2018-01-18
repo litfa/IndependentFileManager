6eab3d35820703937326f5036162740791f3964b
suwings <suwings@outlook.com>
2018-01-18 19:23:22 +0800

    整理

--- controller/auth.js
+++ controller/auth.js
@@ -10,10 +10,6 @@ router.all('/', (req, res) => {
     req.session.fsos = new FileOperateStructure(BASE_DIR, "./");
     req.session.fsoperate = {};
     req.session.fsoperate.tmp = [];
-    req.session.w = "AAA";
-    req.session.ewe = function () {
-        console.log(11111111);
-    };
     req.session.save();
     res.redirect('/public');
 });
